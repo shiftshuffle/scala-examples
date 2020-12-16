6d1e6d725d6a2925b6bc801b9b1766e25acda04f
shiftshuffle <gauss27@ciencias.unam.mx>
2020-12-16 13:40:41 -0600

    MapExample

--- src/main/scala/MapExample.scala
+++ src/main/scala/MapExample.scala
@@ -1,3 +1,12 @@
-object MapExample {
+object MapExample extends App{
+  val example = Map("a" -> 1, "b" -> 2, "c" -> 3)
+  println(example("a"))
+
+  val ratings = Map("Lady in the Water"-> 3.0,
+    "Snakes on a Plane"-> 4.0,
+    "You, Me and Dupree"-> 3.5)
+
+  for ((k,v) <- ratings) println(s"key: $k, value: $v")
+
 
 }
